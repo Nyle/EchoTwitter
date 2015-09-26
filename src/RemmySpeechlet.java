2b78ef4f153cf9db204c2f50129c228792ffebf6
Nyle Rodgers <nyle.rodgers@gmail.com>
2015-09-26 19:27:42 -0400

    add minimal help code

---
+++ src/RemmySpeechlet.java
@@ -0,0 +1,71 @@
+package helloworld;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+import com.amazon.speech.ui.SimpleCard;
+
+public class RemmySpeechlet implements Speechlet {
+        private static final Logger log = LoggerFactory.getLogger(RemySpeechlet.class);
+
+        @Override
+        public void onSessionStarted(final SessionStartedRequest request, final Session session)
+                throws SpeechletException {
+                log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
+                         session.getSessionId());
+                // any initialization logic goes here
+        }
+
+        @Override
+        public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
+                throws SpeechletException {
+                log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
+                         session.getSessionId());
+                return getWelcomeResponse();
+        }
+
+        @Override
+        public SpeechletResponse onIntent(final IntentRequest request, final Session session)
+                throws SpeechletException {
+                log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
+                         session.getSessionId());
+
+                Intent intent = request.getIntent();
+                String intentName = (intent != null) ? intent.getName() : null;
+
+                if ("HelpIntent".equals(intentName)) {
+                        return getHelpResponse();
+                } else {
+                        throw new SpeechletException("Invalid Intent");
+                }
+        }
+
+        @Override
+        public void onSessionEnded(final SessionEndedRequest request, final Session session)
+                throws SpeechletException {
+                log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
+                         session.getSessionId());
+                // any cleanup logic goes here
+        }
+
+        private SpeechletResponse getHelpResponse() {
+                String speechText = "This is the help response";
+
+                // Create the plain text output.
+                PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+                speech.setText(speechText);
+
+                return SpeechletResponse.newTellResponse(speech);
+        }
+}
---
+++ src/RemySpeechletRequestStreamHandler.java
@@ -0,0 +1,21 @@
+package remy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
+
+public final class RemySpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+    private static final Set<String> supportedApplicationIds = new HashSet<String>();
+    static {
+        /*
+         * This Id can be found on https://developer.amazon.com/edw/home.html#/ "Edit" the relevant
+         * Alexa Skill and put the relevant Application Ids in this Set.
+         */
+        supportedApplicationIds.add("amzn1.echo-sdk-ams.app.[unique-value-here]");
+    }
+
+    public RemySpeechletRequestStreamHandler() {
+        super(new RemySpeechlet(), supportedApplicationIds);
+    }
+}

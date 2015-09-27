3c7de62d3a5db127bd54d1f0f4ea1faa25d34bda
Nyle Rodgers <nyle.rodgers@gmail.com>
2015-09-26 23:54:12 -0400

    make build.sh handle errors better

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mvn assembly:assembly -DdescriptorId=jar-with-dependencies package
-echo "================ Uploading ================"
-aws lambda update-function-code --function-name Remy --zip-file fileb://target/remy-1.0-jar-with-dependencies.jar
+mvn assembly:assembly -DdescriptorId=jar-with-dependencies package &&
+    echo "================ Uploading ================" &&
+    aws lambda update-function-code --function-name Remy --zip-file fileb://target/remy-1.0-jar-with-dependencies.jar

6aa5e6d8915c06706bfd3e657f69a9d0807353ab
louismichael <mischamichael4@gmail.com>
2015-10-11 04:42:16 -0400

    delete remy code work with mod hello world Please enter the commit message for your changes. Lines starting

---
+++ src/TwitterSpeechlet.java
@@ -0,0 +1,82 @@
+package src;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+import com.amazon.speech.ui.SimpleCard;
+
+/**
+ * This sample shows how to create a simple speechlet for handling speechlet requests.
+ */
+public class TwitterSpeechlet implements Speechlet {
+    private static final Logger log = LoggerFactory.getLogger(TwitterSpeechlet.class);
+
+    @Override
+    public void onSessionStarted(final SessionStartedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        // any initialization logic goes here
+    }
+
+    @Override
+    public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        return getWelcomeResponse();
+    }
+
+    @Override
+    public SpeechletResponse onIntent(final IntentRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+
+        Intent intent = request.getIntent();
+        String intentName = (intent != null) ? intent.getName() : null;
+
+        if ("NewTweetIntent".equals(intentName)) {
+            return getTweetResponse();
+        } else if ("HelpIntent".equals(intentName)) {
+            return getHelpResponse();
+        } else {
+            throw new SpeechletException("Invalid Intent");
+        }
+    }
+
+    @Override
+    public void onSessionEnded(final SessionEndedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        // any cleanup logic goes here
+    }
+
+
+    /**
+     * Creates a {@code SpeechletResponse} for the help intent.
+     *
+     * @return SpeechletResponse spoken and visual response for the given intent
+     */
+    private SpeechletResponse getHelpResponse() {
+        String speechText = "You can ask me to tweet somthing for you";
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+        
+        return SpeechletResponse.newTellResponse(speech);
+    }
+}

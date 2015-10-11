7f7ad9b2d65fcf49c65978657b7969e35c2dd896
Nyle Rodgers <nyle.rodgers@gmail.com>
2015-10-11 11:23:22 -0400

    fix stuff

--- src/main/java/twitter/EchoTwitterHandler.java
+++ src/main/java/twitter/EchoTwitterHandler.java
@@ -102,15 +102,19 @@ public class EchoTwitterHandler {
                     arr[i+1] = "#" + arr[i+1];
                 }
             }
-            for (int i = 0; i < arr.size() - 2; i++) {
+            for (int i = 0; i < arr.length - 2; i++) {
                 if (arr[i].equals("hash") & arr[i+1].equals("tag")) {
                     arr[i] = "";
                     arr[i+1] = "";
-                    arr[i+2] = "#" + arr[i+1];
+                    arr[i+2] = "#" + arr[i+2];
                 }
             }
-            tweet = String.join(" ", arr);
-            return tweet;
+            for (int i = 0; i < arr.length; i++) {
+                    if (!arr[i].equals("")) {
+                            arr[i] = arr[i] + " ";
+                    }
+            }
+            return String.join("", arr);
         }
 
 }
